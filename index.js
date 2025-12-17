a6b99b0d3d39a9fb7c072ee0504c197da7471c61
Omar Ashour <omar@ashour.ca>
2025-12-16 18:37:56 -0700

    Add db Configuration and Server to Deploy

---
+++ index.js
@@ -0,0 +1,165 @@
+import express from "express";
+import dbConnection from "./db.js";
+import cors from "cors";
+import cookieSession from "cookie-session";
+import process from 'dotenv';
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.set("trust proxy", 1);
+
+app.use(
+  cookieSession({
+    name: process.env.COOKIE_SESSION_NAME,
+    keys: [process.env.COOKIE_SESSION_KEY1, process.env.COOKIE_SESSION_KEY1],
+  })
+);
+
+app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+try {
+  await dbConnection.connect();
+} catch (error) {
+  console.error(error);
+}
+
+app.get("/products", async (req, res) => {
+  try {
+    const [results] = await dbConnection.query("SELECT * FROM Products");
+    res.json(results).status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/checkout", async (req, res) => {
+  const { cart, subtotal } = req.body;
+  const regex = /session=\w+=*;/;
+  const sessionId = req.headers.cookie?.match(regex)[0].slice(8);
+  let orderId;
+
+  try {
+    const [result] = await dbConnection.query(
+      `INSERT INTO Orders (subtotal, session_id) VALUES (${subtotal}, '${sessionId}');`
+    );
+    orderId = result.insertId;
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+
+  cart.forEach(async (item) => {
+    try {
+      await dbConnection.query(
+        `INSERT INTO Purchased_Items (product_id, quantity, order_id, session_id) VALUES (${item.id}, ${item.qtyInCart}, ${orderId}, '${sessionId}')`
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  cart.forEach(async (item) => {
+    try {
+      await dbConnection.query(
+        `UPDATE Products SET stock = stock - ${item.qtyInCart} WHERE id = ${item.id};`
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  req.session.cart = [];
+  res.sendStatus(201);
+});
+
+app.get("/cart", (req, res) => {
+  try {
+    if (!req.session.cart) res.sendStatus(404);
+    res.json(req.session.cart).status(200);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/cart", (req, res) => {
+  try {
+    req.session.cart = [];
+    res.json(req.session.cart).status(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.post("/cart/items", (req, res) => {
+  try {
+    if (!req.session.cart) res.send("No session found").status(400);
+    req.session.cart = [...req.session.cart, { ...req.body, qtyInCart: 1 }];
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.put("/cart/items/:id", (req, res) => {
+  try {
+    if (!req.session.cart) res.send("No session found").status(400);
+    const cart = req.session.cart;
+    const foundProduct = cart.find(
+      (item) => item.id === parseInt(req.params.id)
+    );
+
+    if (!foundProduct) {
+      res.sendStatus(404);
+    }
+
+    const filteredCart = req.session.cart?.filter(
+      (item) => item !== foundProduct
+    );
+    req.session.cart = [
+      ...filteredCart,
+      { ...foundProduct, qtyInCart: foundProduct.qtyInCart + 1 },
+    ];
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.delete("/cart/items/:id", (req, res) => {
+  if (!req.session.cart) res.send("No session found").status(400);
+  try {
+    const cart = req.session.cart;
+    const foundProduct = cart.find(
+      (item) => item.id === parseInt(req.params.id)
+    );
+
+    if (!foundProduct) {
+      res.sendStatus(404);
+    }
+
+    const filteredCart = req.session.cart?.filter(
+      (item) => item !== foundProduct
+    );
+    if (foundProduct.qtyInCart <= 1) {
+      req.session.cart = filteredCart;
+    } else {
+      req.session.cart = [
+        ...filteredCart,
+        { ...foundProduct, qtyInCart: foundProduct.qtyInCart - 1 },
+      ];
+    }
+    res.sendStatus(204);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
+
+app.listen(PORT)
