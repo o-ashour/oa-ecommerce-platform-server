1a64e3d70d86943c59fe0287e60bcaa7f63b7f83
Omar Ashour <omar@ashour.ca>
2026-02-22 22:35:54 -0700

    Use Connection Pool to Keep Connection to DB Open

--- controllers.mjs
+++ controllers.mjs
@@ -1,8 +1,8 @@
-import dbConnection from "./db.mjs";
+import pool from "./db.mjs";
 
 const saveOrder = async (orderId, subtotal) => {
   try {
-    await dbConnection.query(
+    await pool.query(
       `INSERT INTO Orders (id, subtotal) VALUES (${orderId}, ${subtotal});`
     );
   } catch (error) {
@@ -14,7 +14,7 @@ const saveOrder = async (orderId, subtotal) => {
 const savePurchasedItems = async (cart, orderId) => {
   cart.forEach(async (item) => {
     try {
-      await dbConnection.query(
+      await pool.query(
         `INSERT INTO Purchased_Items (product_id, quantity, order_id) VALUES (${item.id}, ${item.qtyInCart}, ${orderId})`
       );
     } catch (error) {
@@ -27,7 +27,7 @@ const savePurchasedItems = async (cart, orderId) => {
 const updateProductStock = async (cart) => {
   cart.forEach(async (item) => {
     try {
-      await dbConnection.query(
+      await pool.query(
         `UPDATE Products SET stock = stock - ${item.qtyInCart} WHERE id = ${item.id};`
       );
     } catch (error) {
@@ -39,7 +39,7 @@ const updateProductStock = async (cart) => {
 
 export const getProducts = async (req, res) => {
   try {
-    const [results] = await dbConnection.query("SELECT * FROM Products");
+    const [results] = await pool.query("SELECT * FROM Products");
     res.json(results).status(200);
   } catch (error) {
     console.error(error);
--- index.mjs
+++ index.mjs
@@ -1,5 +1,4 @@
 import express from "express";
-import dbConnection from "./db.mjs";
 import cors from "cors";
 import dotenv from "dotenv";
 import { getProducts, processOrder } from "./controllers.mjs";
@@ -12,13 +11,6 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
-
-try {
-  await dbConnection.connect();
-} catch (error) {
-  console.error(error);
-  throw new Error("Unable to connect to DB");
-}
 
 app.get("/products", getProducts);
 app.post("/checkout", validateOrder, processOrder);
